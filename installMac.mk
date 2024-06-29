5d662243e79ad93c447695683238fb5ce6b569ea
Csaba Sallai <csabasallai@Csaba-MacBook-Air.local>
2024-06-29 13:42:14 +0200

    NOTICKET Major fixes in the MAC script

--- installMac.mk
+++ installMac.mk
@@ -1,6 +1,6 @@
-.PHONY: all install_homebrew install_node install_dotnet
+.PHONY: all install_homebrew install_nvm install_node add_to_profile install_dotnet
 
-all: install_homebrew install_node install_dotnet
+all: install_homebrew install_nvm add_to_profile install_node install_dotnet
 
 install_homebrew:
 	@echo "Checking for Homebrew..."
@@ -10,9 +10,24 @@ install_homebrew:
 	fi
 	@echo "Homebrew is installed"
 
-install_node:
-	@echo "Installing Node.js LTS 20"
-	brew install node@20
+install_nvm:
+	@echo "Installing NVM using Homebrew..."
+	@if ! brew list nvm >/dev/null 2>&1; then \
+		brew install nvm; \
+	fi
+	@echo "NVM is installed"
+
+add_to_profile:
+	@echo "Adding NVM to profile"
+	@echo 'export NVM_DIR="$$(brew --prefix nvm)"' >> ~/.zshrc
+	@echo '[ -s "$$NVM_DIR/nvm.sh" ] && \. "$$NVM_DIR/nvm.sh"' >> ~/.zshrc
+	@echo '[ -s "$$NVM_DIR/bash_completion" ] && \. "$$NVM_DIR/bash_completion"' >> ~/.zshrc
+
+install_node: install_nvm add_to_profile
+	@echo "Installing Node.js LTS"
+	. "$$(brew --prefix nvm)/nvm.sh"; \
+	nvm install --lts; \
+	nvm use --lts
 
 install_dotnet:
 	@echo "Installing .NET 8.0"
