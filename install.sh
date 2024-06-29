ce0d39ecb90f1d72c086362d118f1267d30c1a6b
csabika98 <sallcsa.csaba8@gmail.com>
2024-06-29 02:35:46 +0200

    NOTICKET Adding LINUX support + some crucial changes in the 'vite.config.js' p4

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-
 set -e
-
 
 sudo apt-get update && sudo apt-get upgrade -y
 
@@ -11,26 +9,31 @@ sudo apt-get install -y curl software-properties-common apt-transport-https wget
 
 
 if [ -d "$HOME/.nvm" ]; then
-    echo "nvm is already installed. Skipping the installation step."
-else
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+    echo "Removing nvm..."
+    rm -rf "$HOME/.nvm"
+    sed -i '/NVM_DIR/d' $HOME/.bashrc
 fi
 
 
-export NVM_DIR="$HOME/.nvm"
-if [ -s "$NVM_DIR/nvm.sh" ]; then
-    . "$NVM_DIR/nvm.sh"  
-fi
-if [ -s "$NVM_DIR/bash_completion" ]; then
-    . "$NVM_DIR/bash_completion"  
-fi
+sudo apt-get install -y nodejs npm
 
 
-command -v nvm >/dev/null 2>&1 || { echo >&2 "NVM is not available. Please log out and back in or source your profile."; exit 1; }
+sudo npm install -g n
 
 
-nvm install --lts
-nvm use --lts
+sudo n lts
+
+
+# sudo n latest
+# sudo n 16.13.0
+
+
+sudo npm cache clean -f
+sudo n prune
+
+
+node --version
+npm --version
 
 
 wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
@@ -43,6 +46,5 @@ sudo apt-get install -y dotnet-sdk-8.0
 
 rm packages-microsoft-prod.deb
 
-node --version
 dotnet --version
 
