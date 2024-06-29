35f6a67925593833e6bc4e52639fd16c9c0f42a4
csabika98 <sallcsa.csaba8@gmail.com>
2024-06-29 02:17:52 +0200

    NOTICKET Adding LINUX support + some crucial changes in the 'vite.config.js' p3

--- install.sh
+++ install.sh
@@ -10,12 +10,23 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y curl software-properties-common apt-transport-https wget
 
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+if [ -d "$HOME/.nvm" ]; then
+    echo "nvm is already installed. Skipping the installation step."
+else
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+fi
 
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"  
+fi
+if [ -s "$NVM_DIR/bash_completion" ]; then
+    . "$NVM_DIR/bash_completion"  
+fi
+
+
+command -v nvm >/dev/null 2>&1 || { echo >&2 "NVM is not available. Please log out and back in or source your profile."; exit 1; }
 
 
 nvm install --lts
@@ -32,6 +43,6 @@ sudo apt-get install -y dotnet-sdk-8.0
 
 rm packages-microsoft-prod.deb
 
-
 node --version
 dotnet --version
+
